c833f03a789b45842a5799e4ff8d79aa2c0f792f
lapitskiy <lapithome@gmail.com>
2026-03-22 16:44:53 +0000

    яяяяяяяяяяяяяяяяяяяяяяяяяяяя

--- app/src/main/java/com/example/sonya_front/SonyaWatchViewModel.kt
+++ app/src/main/java/com/example/sonya_front/SonyaWatchViewModel.kt
@@ -52,7 +52,7 @@ data class SonyaWatchUiState(
 class SonyaWatchViewModel(app: Application) : AndroidViewModel(app) {
     private val _ui = mutableStateOf(
         SonyaWatchUiState(
-            backendUrl = "http://188.243.119.154:443/voice",
+            backendUrl = "http://188.243.119.154:18000/voice",
             lastEvent = "Ожидаю подключения к часам…",
         )
     )
